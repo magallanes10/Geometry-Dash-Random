60edbab3eee4c0fe2725311f7a326f306eae2ca2
Jonell Magallanes <125812726+magallanes10@users.noreply.github.com>
2024-01-06 23:41:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get("/gd", async (req, res) => {
     const randomEdit = edits[randomIndex];
 
     const response = await axios.get(
-      `https://tiktokapi-by-jonell-magallanes--hutchin.repl.co/tiktok/searchvideo?keywords=${encodeURIComponent(
+      `https://9245a8d8-1a9f-4acd-b496-7e355803f2a1-00-3jyrz77tms9n7.asia-b.replit.dev/tiktok/searchvideo?keywords=${encodeURIComponent(
         randomEdit,
       )}`,
       {
